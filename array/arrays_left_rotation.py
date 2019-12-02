1b0d8709f351b7fa4f82a0f7184a8490c9c309aa
Karan Bhatia <karanbhatia2050@gmail.com>
2019-12-02 16:48:12 +0530

    Create arrays_left_rotation.py

---
+++ array/arrays_left_rotation.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+def rotLeft(a, d):
+    # if len(a) == d:
+    #     return a
+
+    # temp=0
+    no_of_rots =  int(d % len(a))
+
+    # for i in range(no_of_rots):
+    #     temp = a[0]
+    #     a = a[1:]
+    #     a.append(temp)
+
+    temp = a[0:no_of_rots]
+    a = a[no_of_rots:]
+    a += temp
+
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
