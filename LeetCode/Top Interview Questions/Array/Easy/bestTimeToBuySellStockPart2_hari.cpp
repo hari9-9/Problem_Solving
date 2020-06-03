56d1054c1f8befdba91aabd1ba352dab66181386
hari9-9 <reachk.hariharan@gmail.com>
2020-06-03 22:56:28 +0530

    cpp solution

---
+++ LeetCode/Top Interview Questions/Array/Easy/bestTimeToBuySellStockPart2_hari.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i = 0;
+        int n = prices.size();
+        int h=prices[0];
+        int l=prices[0];
+        int profit = 0;
+        while(i<(n-1))
+        {
+            while (i<(n-1) && prices[i]>=prices[i+1])
+            {
+                i++;
+            }
+            l = prices[i];
+            while (i<(n-1) && prices[i]<=prices[i+1])
+            {
+                i++;
+            }
+            h = prices[i];
+            profit+=h-l;   
+        }
+        
+        return(profit);
+    }
+};
