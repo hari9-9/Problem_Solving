db07226c0e2364712f51c4656f8e98fa3e0ebde1
Karan Bhatia <karanbhatia2050@gmail.com>
2020-06-06 18:02:47 +0530

    py soln

--- LeetCode/Top Interview Questions/Array/Easy/Rotate Array bhatiak.py
+++ LeetCode/Top Interview Questions/Array/Easy/Rotate Array bhatiak.py
@@ -1,3 +1,10 @@
+"""
+this is my original solution without checking the provided solution. 
+space: 15.5 MB 
+time: 192 ms
+
+"""
+
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         l = len(nums)
