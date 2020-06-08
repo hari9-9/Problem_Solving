403fe51a0f8a92c50275db3ca94e3c895fcb7ff2
hari9-9 <reachk.hariharan@gmail.com>
2020-06-08 14:09:54 +0530

    cpp solution

---
+++ LeetCode/Top Interview Questions/Array/Easy/rotate_image_hari.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n= matrix.size();
+        cout<<n;
+        int i,j,temp;
+        // transpose
+        for (i=0;i<n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        //swap columns
+        for (i=0;i<n;i++)
+        {
+            for(j=0;j<(n/2);j++)
+            {
+                temp = matrix[i][j];
+                matrix[i][j] =matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+                
+                
+            }
+        }
+    }
+};
