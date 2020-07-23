ed0498e307d65fdff77a787cb472e430b20d499f
hari9-9 <reachk.hariharan@gmail.com>
2020-07-23 15:01:41 +0530

    Update Remove_Duplicates_Sorted_array_hari.py

--- LeetCode/Top Interview Questions/Array/Easy/Remove_Duplicates_Sorted_array_hari.py
+++ LeetCode/Top Interview Questions/Array/Easy/Remove_Duplicates_Sorted_array_hari.py
@@ -8,3 +8,20 @@ class Solution:
                 i+=1
                 nums[i]=nums[j]
         return i+1
+    
+    
+    
+    ## second attempt
+    
+    class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        curr =0
+        nex =1
+        while(nex <len(nums)):
+            if nums[curr] != nums[nex]:
+                curr+=1
+                nex+=1                                              
+            else:
+                del nums[nex]
+        
+        return len(nums)
