cb8248d5ae1e30c9e38bfab7ad236e93dd83ce02
hari9-9 <reachk.hariharan@gmail.com>
2020-07-27 12:21:53 +0530

    Update bestTimeToBuySellStockPart2_hari.cpp

--- LeetCode/Top Interview Questions/Array/Easy/bestTimeToBuySellStockPart2_hari.cpp
+++ LeetCode/Top Interview Questions/Array/Easy/bestTimeToBuySellStockPart2_hari.cpp
@@ -24,3 +24,33 @@ public:
         return(profit);
     }
 };
+
+## Second attempt
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int low = prices[0];
+        int high = prices[0];
+        int i=0;
+        int profit = 0;
+        while(i<n-1)
+        {
+            while(i<n-1 && prices[i]>=prices[i+1])
+            {
+                i++;
+            }
+            low=prices[i];
+            while(i<n-1 && prices[i]<=prices[i+1])
+            {
+                i++;
+            }
+            high=prices[i];
+            profit += high - low;
+            
+        }
+        return profit;
+        
+    }
+};
