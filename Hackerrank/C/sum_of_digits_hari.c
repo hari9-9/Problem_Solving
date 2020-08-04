7b09f9d7f9fa6b1b6315bb24a5f42772c1045192
hari9-9 <reachk.hariharan@gmail.com>
2020-08-04 23:15:58 +0530

    Create sum_of_digits_hari.c

---
+++ Hackerrank/C/sum_of_digits_hari.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    scanf("%d", &n);
+    int sum=0;
+    int dig;
+    while(n!=0)
+    {
+        sum+=n%10;
+        n/=10;
+    }
+    printf("%d" , sum);
+    //Complete the code to calculate the sum of the five digits on n.
+    return 0;
+}
