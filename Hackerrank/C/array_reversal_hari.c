d74f1e0fac1c8d1f06de5f5f8155ebd87c797492
hari9-9 <reachk.hariharan@gmail.com>
2020-08-07 01:02:36 +0530

    Create array_reversal_hari.c

---
+++ Hackerrank/C/array_reversal_hari.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    int j;
+    int temp;
+    for(i=0,j=num-1;i<num/2;i++,j--)
+    {
+        temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+
+    }
+    /* Write the logic to reverse the array. */
+
+    for(i = 0; i < num; i++)
+        printf("%d ", *(arr + i));
+    return 0;
+}
