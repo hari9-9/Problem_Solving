901b51f3dce16b7176fa79bd53de285ac1dc59b1
hari9-9 <reachk.hariharan@gmail.com>
2020-08-21 16:26:45 +0530

    second attempt

--- LeetCode/Top Interview Questions/Array/Easy/rotate_image_hari.cpp
+++ LeetCode/Top Interview Questions/Array/Easy/rotate_image_hari.cpp
@@ -28,3 +28,34 @@ public:
         }
     }
 };
+
+
+// second attempt
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int i,j;
+        int temp;
+        int n=matrix.size();
+        for(i=0;i<n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n/2;j++)
+            {
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+        }
+        
+    }
+};
