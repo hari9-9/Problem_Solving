e22e57d534d48bec68982c679ab73e59a0b0bbf4
hari9-9 <reachk.hariharan@gmail.com>
2020-08-21 15:25:55 +0530

    second attempt

--- LeetCode/Top Interview Questions/Array/Easy/move_zeroes_hari.cpp
+++ LeetCode/Top Interview Questions/Array/Easy/move_zeroes_hari.cpp
@@ -14,3 +14,25 @@ public:
         }
     }
 };
+
+// Second attempt
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slow=0;
+        int curr=0;
+        int temp;
+        for(curr=0;curr<nums.size();curr++)
+        {
+            if(nums[curr]!=0)
+            {
+                temp = nums[curr];
+                nums[curr]=nums[slow];
+                nums[slow] = temp;
+                slow++;
+            }
+        }
+        
+    }
+};
