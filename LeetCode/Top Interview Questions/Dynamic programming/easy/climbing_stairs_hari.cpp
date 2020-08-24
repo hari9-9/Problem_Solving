ef5ba8a5e2afebd52079905bf1ce2df568df6042
hari9-9 <reachk.hariharan@gmail.com>
2020-08-24 23:44:59 +0530

    second attempt

--- LeetCode/Top Interview Questions/Dynamic programming/easy/climbing_stairs_hari.cpp
+++ LeetCode/Top Interview Questions/Dynamic programming/easy/climbing_stairs_hari.cpp
@@ -10,3 +10,21 @@ public:
         return ways[n];
     }
 };
+
+
+// second attempt
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int arr[n+1],i;
+        arr[0]=1;
+        arr[1]=1;
+        for(i=2;i<=n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+        
+    }
+};
