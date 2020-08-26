8cdf839cc609365a37a791391b29a2ee77dc1cf9
hari9-9 <reachk.hariharan@gmail.com>
2020-08-26 14:13:36 +0530

    second attempt

--- LeetCode/Top Interview Questions/Dynamic programming/easy/House_Robber_hari.cpp
+++ LeetCode/Top Interview Questions/Dynamic programming/easy/House_Robber_hari.cpp
@@ -59,3 +59,36 @@ public:
     } 
  
 };
+
+
+
+
+
+// second attempt
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==0)
+        {
+            return 0;
+        }
+        if(nums.size()==1)
+        {
+            return nums[0];
+        }
+        if(nums.size()==2)
+        {
+            return nums[1]>nums[0]? nums[1] : nums[0];
+        }
+        int dp[nums.size()];
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        int i;
+        for(i=2;i<nums.size();i++)
+        {
+            dp[i] = nums[i]+dp[i-2] > dp[i-1] ? nums[i]+dp[i-2] : dp[i-1];
+        }
+        return dp[nums.size()-1];
+    }
+};
