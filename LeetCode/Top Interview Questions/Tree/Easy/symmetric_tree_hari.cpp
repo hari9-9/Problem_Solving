bb2e54f75a5e1f4641fb6f7d52a8a5e44a6a3fb7
hari9-9 <reachk.hariharan@gmail.com>
2020-09-12 20:51:22 +0530

    Update symmetric_tree_hari.cpp

--- LeetCode/Top Interview Questions/Tree/Easy/symmetric_tree_hari.cpp
+++ LeetCode/Top Interview Questions/Tree/Easy/symmetric_tree_hari.cpp
@@ -29,3 +29,37 @@ public:
         
     }
 };
+
+// second attempt
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return mirror(root,root);
+        
+    }
+    bool mirror(TreeNode* l , TreeNode* r)
+    {
+        if(l==NULL && r==NULL)
+        {
+            return true;
+        }
+        if(l && r && l->val == r->val)
+        {
+            return mirror(l->right,r->left) && mirror(l->left,r->right);
+        }
+        return false;
+        
+    }
+};
