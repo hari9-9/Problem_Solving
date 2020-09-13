c64ce759e0f5f519ddac4f20de1b078685d3bf61
hari9-9 <reachk.hariharan@gmail.com>
2020-09-13 11:52:54 +0530

    Update kth_largest_element_in_stream_hari.cpp

--- LeetCode/Mock Interviews/arrays/kth_largest_element_in_stream_hari.cpp
+++ LeetCode/Mock Interviews/arrays/kth_largest_element_in_stream_hari.cpp
@@ -31,3 +31,48 @@ public:
  * KthLargest* obj = new KthLargest(k, nums);
  * int param_1 = obj->add(val);
  */
+
+
+
+//priority_Queue implementation
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(pq.size()>=k)
+            {
+                if(nums[i]>pq.top())
+                {
+                    pq.pop();
+                    pq.push(nums[i]);
+                }
+            }
+            else
+                pq.push(nums[i]);
+            
+        }
+        
+    }
+    
+    int add(int val) {
+        if(pq.size()==0 || pq.size()<k)
+            pq.push(val);
+       else if(val>pq.top())
+        {
+            pq.pop();
+            pq.push(val);
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
