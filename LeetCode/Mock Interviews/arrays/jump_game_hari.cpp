52e91fb534140fc2ccf336d4375f370194f2d27e
hari9-9 <reachk.hariharan@gmail.com>
2020-09-17 23:40:47 +0530

    Update jump_game_hari.cpp

--- LeetCode/Mock Interviews/arrays/jump_game_hari.cpp
+++ LeetCode/Mock Interviews/arrays/jump_game_hari.cpp
@@ -13,3 +13,34 @@ public:
         
     }
 };
+
+// backtracking solution wont pass 2 test cases TLE
+
+class Solution {
+public:
+    bool jump(vector<int>& nums, int curr)
+    {
+        if(curr==nums.size()-1)
+        {
+            return true;
+        }
+        bool ans=false;
+        for(int i=1;i<=nums[curr];i++)
+        {
+            if(i+curr<=nums.size()-1)
+            {
+                ans=ans || jump(nums,curr+i);
+            } 
+            if(ans)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        return jump(nums,0);
+        
+    }
+};
