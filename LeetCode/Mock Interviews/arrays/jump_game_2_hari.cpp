b5d974b245baee46aa7b01e09683e88f135409d5
hari9-9 <reachk.hariharan@gmail.com>
2020-10-06 19:46:43 +0530

    Passed 92/92 but tle

--- LeetCode/Mock Interviews/arrays/jump_game_2_hari.cpp
+++ LeetCode/Mock Interviews/arrays/jump_game_2_hari.cpp
@@ -30,3 +30,34 @@ public:
         
     }
 };
+
+
+// Second attempt
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n= nums.size();
+        int i,j;
+        int jumps[n];
+        if(n==0 || nums[0]==0)
+        {
+            return 0;
+        }
+        jumps[0]=0;
+        for(i=1;i<n;i++)
+        {
+            jumps[i]=INT_MAX;
+            for(j=0;j<i;j++)
+            {
+                if(i<=j+nums[j] && jumps[j]!=INT_MAX)
+                {
+                    jumps[i] = min(jumps[i] , jumps[j]+1);
+                    break;
+                }
+            }
+        }
+        
+        return jumps[n-1];
+    }
+};
