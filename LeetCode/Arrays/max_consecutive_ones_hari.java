e35b3b50792c41bc4c829fd342df927368b64b0e
hari9-9 <reachk.hariharan@gmail.com>
2021-11-09 15:52:29 +0530

    Update max_consecutive_ones_hari.java

--- LeetCode/Arrays/max_consecutive_ones_hari.java
+++ LeetCode/Arrays/max_consecutive_ones_hari.java
@@ -28,3 +28,30 @@ class Solution {
         }
     }
 }
+
+
+
+// solution with ternary operator more efficient
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int val = 0;
+        int curr = 0;
+        int i;
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                curr++;
+            }
+            else
+            {
+                if(curr>val)
+                {
+                    val=curr;
+                }
+                curr=0;
+            }
+        }
+        return (curr>val)? curr : val;
+    }
+}
