239631e04c3ed79a621ee8f02ec8f01a22a61dc0
hari9-9 <reachk.hariharan@gmail.com>
2023-06-19 16:02:23 +0530

    move zeroes more optimizations

--- LeetCode/LeetCode_75/move_zeroes_hari.py
+++ LeetCode/LeetCode_75/move_zeroes_hari.py
@@ -30,3 +30,35 @@ class Solution:
                 slow += 1
         for i in range(slow,len(nums)):
             nums[i] = 0
+
+
+
+
+# bad runtime , but good memory 
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in nums:
+            if (i==0):
+                nums.remove(i)
+                nums.append(i)
+        return nums
+    
+
+
+# python optimized 
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) < 2:
+            return -1
+    
+        zeroes = nums.count(0) 
+        
+        nums[:] = [num for num in nums if num != 0]
+        nums += [0] * zeroes
