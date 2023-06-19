dffbd065e819b15b2826f284dffff57c5d0e3f44
hari9-9 <reachk.hariharan@gmail.com>
2023-06-19 15:59:27 +0530

    Move zeores optimized

--- LeetCode/LeetCode_75/move_zeroes_hari.py
+++ LeetCode/LeetCode_75/move_zeroes_hari.py
@@ -10,3 +10,23 @@ class Solution:
                 nums[i] = nums[slow]
                 nums[slow] = temp
                 slow += 1
+
+
+
+
+
+
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        slow = 0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[slow] = nums[i]
+                slow += 1
+        for i in range(slow,len(nums)):
+            nums[i] = 0
