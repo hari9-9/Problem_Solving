e5f30088a4e6818d167fe4d50e419bd781077e5a
hari9-9 <reachk.hariharan@gmail.com>
2024-02-04 16:39:55 +0530

    Create reverse_integer.java

---
+++ LeetCode/Top Interview Questions/Strings/Easy/reverse_integer.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int reverse(int x) {
+        // if(x>(Math.pow(2,31)-1) || x < (-1*Math.pow(2,31)))
+        // {
+        //     return 0;
+        // }
+        boolean flag = false;
+        if(x<0) flag = true;
+        x = Math.abs(x);
+        int ans = 0;
+        int d;
+        while(x>0){
+            d = x%10;
+            if (ans > (Integer.MAX_VALUE - d) / 10) {
+                return 0; 
+            }
+            ans=ans*10+d;
+            x/=10;
+        }
+        // if(ans>(Math.pow(2,31)-1) || ans< (-1*Math.pow(2,31))){
+        //     return 0;
+        // }
+        if(flag) ans*=-1;
+        return ans;
+    }
+}
