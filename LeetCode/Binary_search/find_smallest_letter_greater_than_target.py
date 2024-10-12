afdb874f9e65a3f9bed28ea024257711594ee663
Hari <reachk.hariharan@gmail.com>
2024-10-12 01:22:58 +0100

    smallest letter greater than target

---
+++ LeetCode/Binary_search/find_smallest_letter_greater_than_target.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left, right = 0, len(letters)
+        while left < right:
+            mid = (left + right) // 2
+            if ord(letters[mid]) > ord(target):
+                right = mid
+            else:
+                left = mid + 1
+        return letters[left % len(letters)]
+            
