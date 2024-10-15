960b39127650bc7dec35f85c96445f93a39fe9ca
Hari <reachk.hariharan@gmail.com>
2024-10-16 00:06:41 +0100

    Separate black and white balls

---
+++ LeetCode/Arrays/separate_black_and_white_balls.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        s=list(s)
+        c_z = 0
+        for i in s:
+            if i == '0':
+                c_z+=1
+
+        curr_to_fill =0
+        swaps = 0
+        for i in range(len(s)):
+            ele = s[i]
+            if c_z == 0:
+                break
+            if ele == '0':
+                if curr_to_fill == i:
+                    curr_to_fill +=1
+                    c_z-=1
+                    continue
+                else:
+                    swaps += i-curr_to_fill
+                    curr_to_fill+=1
+                    c_z-=1
+        return swaps
