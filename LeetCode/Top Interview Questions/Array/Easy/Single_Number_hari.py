f9e42f8c78b9442b8928bcb1741c1fc03f35d576
Hari <reachk.hariharan@gmail.com>
2024-10-17 23:54:10 +0100

    resolve

--- LeetCode/Top Interview Questions/Array/Easy/Single_Number_hari.py
+++ LeetCode/Top Interview Questions/Array/Easy/Single_Number_hari.py
@@ -8,3 +8,17 @@ class Solution(object):
         for i in nums:
             a ^= i
         return a
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        hash ={}
+        for i in nums:
+            if i in hash:
+                hash[i]+=1
+            else:
+                hash[i]=1
+        for i in hash:
+            if hash.get(i) == 1:
+                return i
+        return 0
