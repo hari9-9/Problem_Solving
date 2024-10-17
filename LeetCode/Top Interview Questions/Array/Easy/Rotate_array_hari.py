3802ddef0ac57061fb7316c8a60a55c8b03eb5f8
Hari <reachk.hariharan@gmail.com>
2024-10-17 23:42:19 +0100

    resolve

--- LeetCode/Top Interview Questions/Array/Easy/Rotate_array_hari.py
+++ LeetCode/Top Interview Questions/Array/Easy/Rotate_array_hari.py
@@ -3,7 +3,7 @@ class Solution:
         while start < end:
             nums[start], nums[end] = nums[end], nums[start]
             start, end = start + 1, end - 1
-                
+
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
         k %= n
@@ -11,3 +11,38 @@ class Solution:
         self.reverse(nums, 0, n - 1)
         self.reverse(nums, 0, k - 1)
         self.reverse(nums, k, n - 1)
+
+
+
+
+#
+
+
+
+class Solution:
+    def reverse(self, nums: list, start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        #k = k % len(nums)
+        n = len(nums)
+        k = k %n
+        # while k:
+        #     temp = nums[-1]
+        #     for i in range(len(nums)-1,0,-1):
+        #         nums[i] , nums[i-1] = nums[i-1] , nums[i]
+        #     nums[0] = temp
+        #     k-=1
+        # for i in range(k):
+        #     nums.insert(0, nums.pop())
+        self.reverse(nums,0,n-1)
+        self.reverse(nums,0,k-1)
+        self.reverse(nums,k,n-1)
+
+
+        
