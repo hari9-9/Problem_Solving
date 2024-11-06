dea5200fc5038634c6a72358922d35aedbeb5a2a
Hari <reachk.hariharan@gmail.com>
2024-11-06 22:32:17 +0000

    annoying problem remove letter to equalize frequency

---
+++ LeetCode/Hash Table/remove_letter_to_equallize_frequency.py
@@ -0,0 +1,19 @@
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        s = list(word)
+        s.sort()
+        hash = {}
+        for i in s:
+            hash[i] = hash.get(i,0)+1
+        print(hash)
+        for i in hash:
+            hash[i]-=1
+            print(hash)
+            se = set()
+            for j in hash:
+                if hash.get(j):
+                    se.add(hash.get(j))
+            if len(se) == 1:
+                return True
+            hash[i] = hash.get(i,0)+1
+        return False
