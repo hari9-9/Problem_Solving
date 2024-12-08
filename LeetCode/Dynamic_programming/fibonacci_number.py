aa6c915632819c9539f930f3f6932cda8a6148d8
Hari <reachk.hariharan@gmail.com>
2024-12-08 01:37:36 +0000

    fib number bottom up approach

--- LeetCode/Dynamic_programming/fibonacci_number.py
+++ LeetCode/Dynamic_programming/fibonacci_number.py
@@ -14,4 +14,16 @@ class Solution:
         dp = [-1] * (n+1)
         Solution.solve(n,dp)
         return dp[n]
-        
+
+############### Bottom UP Approach
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n ==1 or n==0:
+            return n
+        dp = [0]*(n+1)
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2,n+1):
+            dp[i] = dp [i-1] + dp[i-2]
+        return dp[n]
