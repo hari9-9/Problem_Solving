82609843bf694f8007743050fa4de6b62092e08e
Hari <reachk.hariharan@gmail.com>
2024-12-10 23:33:50 +0000

    in order traversal

---
+++ LeetCode/Binary Trees/binary_tree_inorder.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    @staticmethod
+    def preorder(root,ans):
+        if root:
+            Solution.preorder(root.left,ans)
+            ans.append(root.val)
+            Solution.preorder(root.right,ans)
+
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        Solution.preorder(root,ans)
+        return ans
