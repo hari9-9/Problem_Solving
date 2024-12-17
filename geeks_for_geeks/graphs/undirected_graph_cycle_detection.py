3e0709864604a30ccdb9104bf69fc7f98b38f760
Hari <reachk.hariharan@gmail.com>
2024-12-17 14:55:37 +0000

    cycle detection undirected graph

--- geeks_for_geeks/graphs/undirected_graph_cycle_detection.py
+++ geeks_for_geeks/graphs/undirected_graph_cycle_detection.py
@@ -20,6 +20,18 @@ class Solution:
         return False
 
 
+    def cycleDFS(self,node,parent, visited,adj):
+        visited[node] = True
+        for nb in adj[node]:
+            if not visited[nb]:
+                ans = self.cycleDFS(nb,node,visited,adj)
+                if ans:
+                    return True
+            elif nb!=parent:
+                return True
+        return False
+
+
 
     #Function to detect cycle in an undirected graph.
 	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
@@ -27,7 +39,7 @@ class Solution:
         visited = [False]*V
         for i in range(V):
             if not visited[i]:
-                ans = self.cycleBFS(adj,visited,i)
+                ans = self.cycleDFS(i,-1,visited,adj)
                 if ans:
                     return True
         return False
