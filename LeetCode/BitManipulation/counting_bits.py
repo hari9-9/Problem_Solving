7c257ea2d2672759fa75b206a97f4580f9797f7e
Hari <reachk.hariharan@gmail.com>
2025-01-10 23:10:22 +0000

    counting bits

--- LeetCode/BitManipulation/counting_bits.py
+++ LeetCode/BitManipulation/counting_bits.py
@@ -12,3 +12,15 @@ class Solution:
         for i in range(len(ans)):
             ans[i] = self.solve(i)
         return ans
+
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0]*(n+1)
+        off = 1
+        for i in range(1,n+1):
+            if off *2 == i:
+                off=i
+            dp[i] = 1+ dp[i-off]
+        return dp
