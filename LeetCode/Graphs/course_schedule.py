edd1c7767040d13191d0c03438425829e1fcfb29
Hari <reachk.hariharan@gmail.com>
2025-01-12 15:23:40 +0000

    solving course schedule using graph coloring

--- LeetCode/Graphs/course_schedule.py
+++ LeetCode/Graphs/course_schedule.py
@@ -22,4 +22,32 @@ class Solution:
             if not dfs(crs):
                 return False
         return True
-        
+
+
+
+### graph coloring:
+class Solution:
+
+    def color(self,graph,visited,i):
+        if visited[i]==2:
+            return True
+        visited[i] = 2
+        for n in graph[i]:
+            if visited[n] !=1:
+                if self.color(graph,visited,n):
+                    return True
+        visited[i] = 1
+        return False
+
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visited = [0]*numCourses
+        graph = {i:[] for i in range(numCourses)}
+
+        for crs , pre in prerequisites:
+            graph[crs].append(pre)
+
+        for i in range(numCourses):
+            if visited[i]==0:
+                if self.color(graph,visited,i):
+                    return False
+        return True
